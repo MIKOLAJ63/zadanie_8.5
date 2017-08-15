161c300f1f8820d0271f9e13a11d16dff8c04120
MIKOLAJ63 <MIKOLAJ63>
2017-08-15 12:19:43 +0200

    Triangle area var

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+var a = prompt('base value');
+var h = prompt('height value');
+var triangleArea=a*h/2;
+alert('Triangle field with base a:'+ a +' and height h:'+ h +' is equal to:'+ triangleArea +'');
+console.log('Triangle field with base a:'+ a +' and height h:'+ h +' is equal to: ' + triangleArea +'');
+// scripts.js
+var a = 5
+var h = 10
+var triangleArea=a*h/2;
+alert('Triangle field with base a:'+ a +' and height h:'+ h +' is equal to:'+ triangleArea +'');
+console.log('Triangle field with base a:'+ a +' and height h:'+ h +' is equal to:'+ triangleArea +'');
